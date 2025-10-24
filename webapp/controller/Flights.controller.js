8599462a28f0bbf94d654927ef70dcc54a2c8e3c
Mayko Facci <mk0f@outlook.com>
2025-10-24 07:40:21 -0300

    First commit

---
+++ webapp/controller/Flights.controller.js
@@ -0,0 +1,12 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+],
+function (Controller) {
+    "use strict";
+
+    return Controller.extend("student.com.sap.training.advancedsapui5.fullscreen.controller.Flights", {
+        onInit: function () {
+
+        }
+    });
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"studentcomsaptrainingadvanced_sapui5/fullscreen/test/unit/controller/Carrier.controller"
+], function () {
+	"use strict";
+});

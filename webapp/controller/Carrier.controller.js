a675dcfe091532770e737862b85a1d64861fa2b6
Mayko Facci <mk0f@outlook.com>
2025-10-25 08:00:01 -0300

    Implementing navigation in a full-screen application

--- webapp/controller/Carrier.controller.js
+++ webapp/controller/Carrier.controller.js
@@ -1,12 +1,23 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller"
 ],
-function (Controller) {
-    "use strict";
+    function (Controller) {
+        "use strict";
 
-    return Controller.extend("student.com.sap.training.advancedsapui5.fullscreen.controller.Carrier", {
-        onInit: function () {
+        return Controller.extend("student.com.sap.training.advancedsapui5.fullscreen.controller.Carrier", {
+            onInit: function () {
 
-        }
+            },
+
+            getRouter: function () {
+                return sap.ui.core.UIComponent.getRouterFor(this);
+            },
+
+            onPress: function (oEvent) {
+                var sCarrid = oEvent.getSource().getBindingContext().getProperty("Carrid");
+                this.getRouter().navTo("flights", {
+                    carrid: sCarrid
+                }, false);
+            }
+        });
     });
-});
